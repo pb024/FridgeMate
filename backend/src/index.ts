935c51d49b5db1ca21682415caa3ee31bb7aafd9
pb597 <pb597@cornell.edu>
2026-06-02 16:37:13 -0400

    initial commit

---
+++ backend/src/index.ts
@@ -0,0 +1,28 @@
+import express from "express";
+import cors from "cors";
+import { ENV } from "./config/env"
+import { clerkMiddleware } from "@clerk/express";
+
+const app = express();
+
+app.use(cors({ origin: ENV.FRONTEND_URL, credentials: true }));
+// `credentials: true` allows the frontend to send cookies to the backend so that we can authenticate the user
+app.use(clerkMiddleware()); // auth object attached to req
+app.use(express.json()); // parses JSON request bodies.
+app.use(express.urlencoded({ extended: true })); // parses form data (like HTML forms)
+
+
+// 
+app.get("/", (req, res) => {
+    res.json({
+        message: "Welcome to FridgeMate!",
+        endpoints: {
+            users: "/api/users",
+            inventory: "/api/inventory",
+            recipes: "/api/recipes",
+        }
+    });
+});
+
+// starts the server
+app.listen(ENV.PORT, () => console.log("Server is up and running on PORT:", ENV.PORT));
